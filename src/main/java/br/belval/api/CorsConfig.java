eae631a28f226a0234656489d166e2c308d561c3
Elisa <rm101578@estudante.fieb.edu.br>
2026-05-13 22:00:20 -0300

    Ligação do back end com o front end

---
+++ src/main/java/br/belval/api/CorsConfig.java
@@ -0,0 +1,25 @@
+package br.belval.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * CORS = Cross Origin Resource Sharing/
+ *        Compartilhamento Cruzado de Origem de Recurso
+ */
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedOrigins("http://localhost:5173") 
+		.allowedMethods("GET" , "POST" , "PUT" , "DELETE" ,"OPTIONS" , "PATH")
+		.allowedHeaders("*");
+	}
+
+
+
+}
